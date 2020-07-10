eb35145c4cfe8cf2e8bc88db837570fd7d1b411b
Viacheslav Denysko <deniskosv@gmail.com>
2020-07-10 14:17:23 +0300

    done day3

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
-'use strict';
+// 'use strict';
 
-const dB = [];
+const dB = JSON.parse(localStorage.getItem('awito')) || [];
 
 const modalAdd = document.querySelector('.modal__add'),
 	addAd = document.querySelector('.add__ad'),
@@ -8,12 +8,23 @@ const modalAdd = document.querySelector('.modal__add'),
 	modalSubmit = document.querySelector('.modal__submit'),
 	modalItem = document.querySelector('.modal__item'),
 	catalog = document.querySelector('.catalog'),
-	modalBtnWarning = document.querySelector('.modal__btn-warning');
+	modalBtnWarning = document.querySelector('.modal__btn-warning'),
+	modalFileInput = document.querySelector('.modal__file-input'),
+	modalFileBtn = document.querySelector('.modal__file-btn'),
+	modalImageAdd = document.querySelector('.modal__image-add'),
+	modalContent = document.querySelector('.modal__content');
+
+const textFileBtn = modalFileBtn.textContent,
+	srcModalImg = modalImageAdd.src;
 
 const elemsModalSubmit = [...modalSubmit.elements].filter(elem => elem.tagName !== 'BUTTON');
 
+const infoPhoto = {};
+
+const saveDb = () => localStorage.setItem('awito', JSON.stringify(dB));
+
 const checkForm = () => {
-	const validForm = elemsModalSubmit.every(elem => elem.value)
+	const validForm = elemsModalSubmit.every(elem => elem.value);
 	modalBtnSubmit.disabled = !validForm;
 	modalBtnWarning.style.display = validForm ? 'none' : '';
 };
@@ -21,28 +32,77 @@ const checkForm = () => {
 const closeModal = event => {
 	const target = event.target;
 
-	if (target.classList.contains('modal__close') || target.classList.contains('modal') || event.code === 'Escape') {
+	if (target.classList.contains('modal__close') ||
+	target.classList.contains('modal') ||
+	event.code === 'Escape') {
 		modalAdd.classList.add('hide');
 		modalItem.classList.add('hide');
 		modalSubmit.reset();
+		modalImageAdd.src = srcModalImg;
+		modalFileBtn.textContent = textFileBtn;
 		document.removeEventListener('keydown', closeModal);
 		checkForm();
 	}
 };
 
+const renderCard = () => {
+	catalog.textContent = '';
+
+	dB.forEach((item, i) => {
+		catalog.insertAdjacentHTML('beforeend', `
+			<li class="card" data-id="${i}">
+				<img class="card__image" src="data:image/jpeg;base64,${item.image}" alt="test">
+				<div class="card__description">
+					<h3 class="card__header">${item.nameItem}</h3>
+					<div class="card__price">${item.costItem}</div>
+				</div>
+			</li>
+		`);
+	});
+};
+
+
+
+modalFileInput.addEventListener('change', event => {
+	const target = event.target;
+
+	const reader = new FileReader();
+
+	const file = target.files[0];
+
+	infoPhoto.filename = file.name;
+	infoPhoto.size = file.size;
+
+	reader.readAsBinaryString(file);
+
+	reader.addEventListener('load', event => {
+		if (infoPhoto.size < 200000) {
+			modalFileBtn.textContent = infoPhoto.filename;
+			infoPhoto.base64 = btoa(event.target.result);
+			modalImageAdd.src = `data:image/jpeg;base64,${infoPhoto.base64}`;
+		} else {
+			modalFileBtn.textContent = 'Файл не должен превышать 200 кБ';
+			modalFileInput.value = '';
+			checkForm();
+		}
+	});
+
+});
+
 modalSubmit.addEventListener('input', checkForm);
 
 modalSubmit.addEventListener('submit', event => {
 	event.preventDefault();
-	
+
 	const itemObj = {};
 	for (const elem of elemsModalSubmit) {
 		itemObj[elem.name] = elem.value;
 	}
+	itemObj.image = infoPhoto.base64;
 	dB.push(itemObj);
-	modalSubmit.reset();
-	closeModal({target: modalAdd});
-
+	closeModal({ target: modalAdd });
+	saveDb();
+	renderCard();
 });
 
 addAd.addEventListener('click', () => {
@@ -61,3 +121,6 @@ catalog.addEventListener('click', event => {
 
 modalAdd.addEventListener('click', closeModal);
 modalItem.addEventListener('click', closeModal);
+
+renderCard();
+
