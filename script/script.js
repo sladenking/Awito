d6d92faafc921a62a6d8c5a5a65ea89bf82a57b7
Viacheslav Denysko <deniskosv@gmail.com>
2020-07-11 15:40:49 +0300

    done search, filter category, render modal card

--- script/script.js
+++ script/script.js
@@ -2,6 +2,8 @@
 
 const dB = JSON.parse(localStorage.getItem('awito')) || [];
 
+let counter = dB.length;
+
 const modalAdd = document.querySelector('.modal__add'),
 	addAd = document.querySelector('.add__ad'),
 	modalBtnSubmit = document.querySelector('.modal__btn-submit'),
@@ -12,7 +14,13 @@ const modalAdd = document.querySelector('.modal__add'),
 	modalFileInput = document.querySelector('.modal__file-input'),
 	modalFileBtn = document.querySelector('.modal__file-btn'),
 	modalImageAdd = document.querySelector('.modal__image-add'),
-	modalContent = document.querySelector('.modal__content');
+	modalImageItem = document.querySelector('.modal__image-item'),
+	modalHeaderItem = document.querySelector('.modal__header-item'),
+	modalStatusItem = document.querySelector('.modal__status-item'),
+	modalDescriptionItem = document.querySelector('.modal__description-item'),
+	modalCostItem = document.querySelector('.modal__cost-item'),
+	searchInput = document.querySelector('.search__input'),
+	menuContainer = document.querySelector('.menu__container');
 
 const textFileBtn = modalFileBtn.textContent,
 	srcModalImg = modalImageAdd.src;
@@ -45,12 +53,12 @@ const closeModal = event => {
 	}
 };
 
-const renderCard = () => {
+const renderCard = (DB = dB) => {
 	catalog.textContent = '';
 
-	dB.forEach((item, i) => {
+	DB.forEach(item => {
 		catalog.insertAdjacentHTML('beforeend', `
-			<li class="card" data-id="${i}">
+			<li class="card" data-id-item="${item.id}">
 				<img class="card__image" src="data:image/jpeg;base64,${item.image}" alt="test">
 				<div class="card__description">
 					<h3 class="card__header">${item.nameItem}</h3>
@@ -61,7 +69,16 @@ const renderCard = () => {
 	});
 };
 
+searchInput.addEventListener('input', () => {
 
+	const valueSearch = searchInput.value.trim().toLowerCase();
+	if (valueSearch.length > 2) {
+		const result = dB.filter(item => item.nameItem.toLowerCase().includes(valueSearch) ||
+															item.descriptionItem.toLowerCase().includes(valueSearch));
+		renderCard(result);
+	}
+
+});
 
 modalFileInput.addEventListener('change', event => {
 	const target = event.target;
@@ -97,6 +114,7 @@ modalSubmit.addEventListener('submit', event => {
 	const itemObj = {};
 	for (const elem of elemsModalSubmit) {
 		itemObj[elem.name] = elem.value;
+		itemObj.id = counter++;
 	}
 	itemObj.image = infoPhoto.base64;
 	dB.push(itemObj);
@@ -113,14 +131,33 @@ addAd.addEventListener('click', () => {
 
 catalog.addEventListener('click', event => {
 	const target = event.target;
+	const card = target.closest('.card');
+
 	if (target.closest('.card')) {
+		const item = dB.find(obj => obj.id === +card.dataset.idItem);
+
+		modalImageItem.src = `data:image/jpeg;base64,${item.image}`;
+		modalHeaderItem.textContent = item.nameItem;
+		modalStatusItem.textContent = item.status === 'new' ? 'Новый' : 'Б/У';
+		modalDescriptionItem.textContent = item.descriptionItem;
+		modalCostItem.textContent = item.costItem;
+
 		modalItem.classList.remove('hide');
 		document.addEventListener('keydown', closeModal);
 	}
+});
+
+menuContainer.addEventListener('click', event => {
+	const target = event.target;
+
+	if (target.tagName === 'A') {
+		const result = dB.filter(item => item.category === target.dataset.category);
+		renderCard(result);
+	}
+
 });
 
 modalAdd.addEventListener('click', closeModal);
 modalItem.addEventListener('click', closeModal);
 
 renderCard();
-
